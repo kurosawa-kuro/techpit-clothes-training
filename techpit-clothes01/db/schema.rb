2dc146e49d9bb9bd3ef48214ebbe988e7773b517
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-29 13:29:32 +0000

    add

--- techpit-clothes01/db/schema.rb
+++ techpit-clothes01/db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_130608) do
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "image", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
